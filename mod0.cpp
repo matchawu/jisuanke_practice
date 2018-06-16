d27900fb6833d0fbe9697e8ed3e85f2795721433
MatchaWu <matchawu@users.noreply.github.com>
2018-06-16 14:08:39 +0800

    Create mod0.cpp

---
+++ mod0.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int M,N;
+    cin>> M>>N;
+    if(M%N == 0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
